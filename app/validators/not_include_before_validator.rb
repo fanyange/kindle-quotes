f0c119b6bc17f05e6cd4bc1284878663ea973f6e
Tom Mao <mr.fanyange@gmail.com>
2015-08-31 01:02:51 +0800

    Delete the useless validator.

--- app/validators/not_include_before_validator.rb
+++
@@ -1,7 +0,0 @@
-class NotIncludeBeforeValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if Quote.where("instr(?, #{attribute}) > 0", value).any?
-      record.errors.add attribute, "includes before"
-    end
-  end
-end
