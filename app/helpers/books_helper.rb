34e300fd20b1b694d7cae3e41c58a554d10b191b
Tom Mao <mr.fanyange@gmail.com>
2015-08-31 21:09:20 +0800

    simplify the SQL query

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,7 +2,7 @@ require "open-uri"
 
 module BooksHelper
   def frequency_data
-    query = "select date, title as favorite, max(count) as max_count, sum(count) as sum_count from (select Date(quotes.created_at) as date, count(*) as count, title from quotes INNER JOIN books ON books.id = quotes.book_id group by Date(quotes.created_at), book_id) as s1 group by date;"
+    query = "select date, title as favorite, max(count) as max_count, sum(count) as sum_count from (select Date(quotes.created_at) as date, count(*) as count, title from quotes INNER JOIN books ON books.id = quotes.book_id group by Date(quotes.created_at), book_id) group by date;"
     Quote.connection.select_all(query)
   end
 end
